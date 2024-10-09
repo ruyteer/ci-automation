f08fed7a9dd396b62689ae436703abe465795d46
Ruyter Araujo Rocha <133131115+ruyteer@users.noreply.github.com>
2024-10-09 11:55:07 -0300

    gerar ci compras

--- functions/generate_ci.py
+++ functions/generate_ci.py
@@ -44,4 +44,37 @@ def preencher_modelo_word(data, word_output_path):
 
    
 
+def preencher_modelo_compras(data, word_output_path):
+    # Carregar o template
+    doc = DocxTemplate("modelo_compras.docx")
 
+    # Definir o contexto
+    context = {
+        'COD_ARQ': data['cod_arq'],
+        'DATA': data['date'],
+        'CODIGO': data['codigo_requisicao'],
+        'SOLICITANTE': data['solicitante'],
+        'LOCAL_ENTREGA': data['local_entrega'],
+        'VALOR_TOTAL': data['valor_total'],
+        'JUSTIFICATIVA': data['justificativa'],
+        'MOTIVO': data['motivo'],
+        'EMPRESA': data['empresa'],
+        'ITENS': []
+    }
+
+    # Preencher itens
+    for item in data['itens']:
+        formatted_item = f"{item['quantidade']} | {item['descricao']} | R$ {item['preco']}"
+        context['ITENS'].append(formatted_item)
+
+    # Renderiza o template Word
+    doc.render(context)
+
+    # Salva o documento Word preenchido
+    doc.save(word_output_path)
+
+    # Extrair o nome do arquivo Word
+    # word_filename = os.path.basename(word_output_path)
+
+    # Converte o arquivo Word para PDF
+    # pdf_filename = convert_to_pdf(folder="uploads", source=word_filename, timeout=15)
--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, send_from_directory, send_fil
 import os
 from functions.get_req import get_data_from_req
 from functions.generate_ci import preencher_modelo_word
+from functions.generate_ci import preencher_modelo_compras
 from functions.get_emails import ler_emails
 import zipfile
 
@@ -21,6 +22,21 @@ def home():
             return render_template("index.html", data=data, pdf_filename=uploaded_file.filename)
     return render_template("index.html", data=None, pdf_filename=None)
 
+
+@app.route("/compras", methods=["GET", "POST"])
+def compras():
+    if not os.path.exists('uploads'):
+        os.makedirs('uploads')
+
+    if request.method == "POST":
+        uploaded_file = request.files['file']
+        if uploaded_file.filename.endswith('.pdf'):
+            pdf_path = os.path.join("uploads", uploaded_file.filename)
+            uploaded_file.save(pdf_path)
+            data = get_data_from_req(pdf_path)
+            return render_template("compras.html", data=data, pdf_filename=uploaded_file.filename)
+    return render_template("compras.html", data=None, pdf_filename=None)
+
 @app.route("/emails", methods=["GET"])
 def emails():
     if not os.path.exists('uploads'):
@@ -35,6 +51,45 @@ def emails():
 @app.route('/uploads/<filename>')
 def upload_file(filename):
     return send_from_directory('uploads', filename)
+
+@app.route('/gerar_ci_compras', methods=["POST"])
+def gerar_ci():
+    # Pegar os dados do formulário
+    data = {
+        'codigo_requisicao': request.form['codigo_requisicao'],
+        'solicitante': request.form['solicitante'],
+        'local_entrega': request.form['local_entrega'],
+        'justificativa': request.form['justificativa'],
+        'valor_total': request.form['valor_total'],
+        'motivo': request.form['motivo'],
+        'cod_arq': request.form['cod_arq'],
+        'date': request.form['data'],
+        'name_file': request.form['name_file'],
+        'empresa': request.form['empresa'],
+        'itens': []
+    }
+
+    # Pegar os itens do formulário
+    quantidades = request.form.getlist('quantidade[]')
+    descricoes = request.form.getlist('descricao[]')
+    precos = request.form.getlist('preco[]')
+
+    for i in range(len(quantidades)):
+        item = {
+            'quantidade': quantidades[i],
+            'descricao': descricoes[i],
+            'preco': precos[i]
+        }
+        data['itens'].append(item)
+
+    name_file = f"{data['name_file']}.docx"
+    word_output_path = os.path.join("uploads", name_file)
+
+    # Preencher o modelo Word e converter para PDF
+    preencher_modelo_compras(data, word_output_path)
+
+    # Retornar o arquivo ZIP para o usuário baixar
+    return send_file(word_output_path, as_attachment=True)
 
 @app.route('/gerar_ci', methods=["POST"])
 def gerar_ci():
